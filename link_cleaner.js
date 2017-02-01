ac2bb7280017ab4003679bf2f06fa1acaf0a588b
Mark Tai <mark@marktai.com>
2017-02-01 15:18:31 -0800

    added slack cleaning too

--- link_cleaner.js
+++ link_cleaner.js
@@ -1,18 +1,33 @@
 $("html").on("mouseenter", "a", function(){
     var href = $(this).attr("href");
+
+    console.log("Cleaning " + href);
     
     // reddit fix
     var outbound = $(this).attr("data-outbound-url");
-    if (outbound.indexOf("out.reddit.com") > -1) {
+    if (outbound && outbound.indexOf("out.reddit.com") > -1) {
         var cleanLink = href;
         $(this).attr("data-outbound-url", cleanLink);
         return;
     }
 
     // facebook fix
-    if (href.indexOf("facebook.com/l?") > -1) {
+    if (href && (
+        href.indexOf("l.facebook.com") > -1 ||
+        href.indexOf("facebook.com/l?") > -1
+    )) {
         var cleanLink = $(this).text();
         $(this).attr("href", cleanLink);
+        return;
+    }
+
+    // slack fix
+    var onclick = $(this).attr("onclick");
+    if (onclick && onclick.indexOf("this.href=") > -1) {
+        var cleanLink = $(this).attr("data-referer-original-href");
+        $(this).attr("href", cleanLink);
+        $(this).attr("onclick", "this.href='"+cleanLink+"'");
+        $(this).attr("onmouseover", "this.href='"+cleanLink+"'");
         return;
     }
     
@@ -21,7 +36,7 @@ $("html").on("mouseenter", "a", function(){
 var cleanGoogleLink = function(moneyThis) {
     // google fix on left click
     var href = moneyThis.attr("href");
-    if (href.indexOf("/url?") > -1) {
+    if (moneyThis.attr("data-href") && href.indexOf("/url?") > -1) {
         var cleanLink = moneyThis.attr("data-href");
         if (cleanLink) {
             moneyThis.attr("href", cleanLink);
